9b8cb007a41145899138be7c845147f0d4ab958b
Felipe Oliveira <lipefan0@gmail.com>
2025-09-12 09:58:13 -0300

    commit inicial

---
+++ src/main/java/br/com/upvisibility/hub_bling/HubBlingApplication.java
@@ -0,0 +1,13 @@
+package br.com.upvisibility.hub_bling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubBlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubBlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/upvisibility/hub_bling/HubBlingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upvisibility.hub_bling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubBlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
